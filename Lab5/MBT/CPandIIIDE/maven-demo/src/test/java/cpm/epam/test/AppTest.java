971136e16e40c8c94f56adb2d47afa1eacaf6604
dimaqqw <dimaaqqw@mail.ru>
2023-10-03 08:41:11 +0300

    l5 in proccess

---
+++ Lab5/MBT/CPandIIIDE/maven-demo/src/test/java/cpm/epam/test/AppTest.java
@@ -0,0 +1,20 @@
+package cpm.epam.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
